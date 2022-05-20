b28e4498870067982ad537306173fc355d29b9a1
Olamide Omolola <omololaolamidex@gmail.com>
2022-05-20 12:50:03 +0200

    added mqtt publishing and http publishing

---
+++ hx711py/reading.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+import errno
+import os
+import json
+
+
+import RPi.GPIO as GPIO
+from hx711 import HX711
+
+class Reading:
+    """Class for retrieving readings from devices."""
+
+    def _init_(self, reference_unit=None):
+        self.hx = HX711(5, 6)
+        self.hx.set_reading_format("MSB", "MSB")
+        if reference_unit:
+            set_reference_unit(reference_unit)
+        self.hx_reset()
+        self.hx.tare()
+
+        print("Tare done! Add weight now...")
+
+    def set_reference_unit(self, reference_unit):
+        self.hx.set_reference_unit(reference_unit)
+
+    def get_readings(self):
+        """Write the readings for the current context."""
+
+        sensor_id = os.getenv('SENSOR_ID', '')
+        UUID = os.environ.get('RESIN_DEVICE_UUID')[:7] # First seven chars of device UUID
+        
+        reading = {}
+        #while True:
+        try:
+            val = self.hx.get_weight(5)
+            
+            if sensor_id:
+                reading.update({"sensor_id": sensor_id}) 
+            reading.update({"short_uuid": UUID})
+            reading.update({"weight": val})
+
+            print(val)
+
+            self.hx.power_down()
+            self.hx.power_up()
+            time.sleep(0.1)
+
+        except (KeyboardInterrupt, SystemExit):
+            self.clean_and_exit()
+        return reading
+
+
+    def clean_and_exit():
+        print("Cleaning...")
+
+        GPIO.cleanup()
+            
+        print("Bye!")
+        sys.exit()
---
+++ hx711py/sensor.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+import sys
+import errno
+import os
+import json
+import time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import paho.mqtt.client as mqtt
+import socket
+import threading
+import requests
+
+from reading import Reading
+
+
+def mqtt_detect():
+    
+    # Use the supervisor api to get services
+    # See https://www.balena.io/docs/reference/supervisor/supervisor-api/
+    
+    address = os.getenv('BALENA_SUPERVISOR_ADDRESS', '')
+    api_key = os.getenv('BALENA_SUPERVISOR_API_KEY', '')
+    app_name = os.getenv('BALENA_APP_NAME', '')
+
+    url = "{0}/v2/applications/state?apikey={1}".format(address, api_key)
+
+    try:
+        r = requests.get(url).json()
+    except Exception as e:
+        print("Error looking for MQTT service: {0}".format(str(e)))
+        return False
+    else:
+        services = r[app_name]['services'].keys()
+
+        if "mqtt" in services:
+            return True
+        else:
+            return False
+    
+
+class balenaSense():
+    
+    def _init_(self):
+        self.sensor = Reading()
+        #self.sensor = Reading(1)
+
+    def sample(self):
+        return self.sensor.get_readings()
+
+# Simple webserver
+def background_web(server_socket):
+    balenasense = balenaSense()
+    while True:
+        # Wait for client connections
+        client_connection, client_address = server_socket.accept()
+
+        # Get the client request
+        request = client_connection.recv(1024).decode()
+        print(request)
+
+        # Send HTTP response
+        response = 'HTTP/1.0 200 OK\n\n'+ json.dumps(balenasense.sample())
+        client_connection.sendall(response.encode())
+        client_connection.close()
+
+
+
+if __name__ == "__main__":
+
+    mqtt_address = os.getenv('MQTT_ADDRESS', 'none')
+    use_httpserver = os.getenv('ALWAYS_USE_HTTPSERVER', 0)
+    publish_interval = os.getenv('MQTT_PUB_INTERVAL', '8')
+    publish_topic = os.getenv('MQTT_PUB_TOPIC', 'sensors')
+    
+    try:
+        interval = float(publish_interval)
+    except Exception as e:
+        print("Error converting MQTT_PUB_INTERVAL: Must be integer or float! Using default.")
+        interval = 8
+        
+    if use_httpserver == "1":
+        enable_httpserver = "True"
+    else:
+        enable_httpserver = "False"
+    
+
+    if mqtt_detect() and mqtt_address == "none":
+        mqtt_address = "mqtt"
+
+    if mqtt_address != "none":
+        print("Starting mqtt client, publishing to {0}:1883".format(mqtt_address))
+        print("Using MQTT publish interval: {0} sec(s)".format(interval))
+        client = mqtt.Client()
+        try:
+            client.connect(mqtt_address, 1883, 60)
+        except Exception as e:
+            print("Error connecting to mqtt. ({0})".format(str(e)))
+            mqtt_address = "none"
+            enable_httpserver = "True"
+        else:
+            client.loop_start()
+            balenasense = balenaSense()
+    else:
+        enable_httpserver = "True"
+
+    if enable_httpserver == "True":
+        SERVER_HOST = '0.0.0.0'
+        SERVER_PORT = 7575
+
+        # Create socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((SERVER_HOST, SERVER_PORT))
+        server_socket.listen(1)
+        print("HTTP server listening on port {0}...".format(SERVER_PORT))
+
+        t = threading.Thread(target=background_web, args=(server_socket,))
+        t.start()
+
+    while True:
+        if mqtt_address != "none":
+            client.publish(publish_topic, json.dumps(balenasense.sample()))
+        time.sleep(interval)
